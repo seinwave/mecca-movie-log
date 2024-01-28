d188629a18ce1cfa1f1e378a1321f8b79bf75a28
Matt Seidholz <mseidholz@gmail.com>
2024-01-28 15:31:02 -0500

    make non null added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_28_202636) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_28_202936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_28_202636) do
 
   create_table "ratings", force: :cascade do |t|
     t.date "watched_date"
-    t.integer "score"
+    t.integer "score", null: false
     t.bigint "user_id", null: false
     t.bigint "movie_id", null: false
     t.datetime "created_at", null: false
