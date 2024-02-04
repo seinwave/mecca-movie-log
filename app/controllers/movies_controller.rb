1c8299823025d96e29fb664ecd76d50df20b2cec
Matt Seidholz <mseidholz@gmail.com>
2024-02-04 07:11:35 -0500

    reverse sorting, mobile styles

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ include RatingsHelper
 
   def self.sort(movies)
     movies.sort_by { |movie| 
-    movie.ratings.first.watched_date || movie.ratings.last.watched_date }
+    movie.ratings.first.watched_date || movie.ratings.last.watched_date }.reverse
   end
 
 end
