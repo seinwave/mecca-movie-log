1f0306086a335163cfd7470952582f601f19a68e
Matt Seidholz <mseidholz@gmail.com>
2024-02-11 16:33:45 -0500

    reconfigure specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,12 @@
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
+  # assertions if you prefer
+
+  # seed test db
+  config.before(:suite) do
+    Rails.application.load_seed # loading seeds
+  end
 
   config.filter_gems_from_backtrace 'rspec-rails', 'factory_bot'
 
