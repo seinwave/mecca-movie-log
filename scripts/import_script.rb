e82a35fd7fc0f3c505de58329b4580a8faeb55cd
Matt Seidholz <mseidholz@gmail.com>
2024-02-11 16:26:12 -0500

    refactor import script

--- scripts/import_script.rb
+++ scripts/import_script.rb
@@ -67,21 +67,21 @@ class MovieImporter
   end
 
   def import_movies(file_path)
-  file_path = File.expand_path(file_path)
+    file_path = File.expand_path(file_path)
 
-  puts "importing movies from #{file_path}!"
-  CSV.foreach(file_path, headers: true) do |row|
-    # Skip if the row is empty or the title is empty
-    puts 'ROW!', row['TITLE']
-    next unless is_valid_title?(row['TITLE'])
+    puts "importing movies from #{file_path}!"
+    CSV.foreach(file_path, headers: true) do |row|
+      # Skip if the row is empty or the title is empty
+      puts 'ROW!', row['TITLE']
+      next unless is_valid_title?(row['TITLE'])
 
-    puts "importing #{row['TITLE']}"
+      puts "importing #{row['TITLE']}"
 
-    # Insert movie if it doesn't exist
-    movie_title = row['TITLE']
-    Movie.find_or_create_by(title: movie_title)
+      # Insert movie if it doesn't exist
+      movie_title = row['TITLE']
+      Movie.find_or_create_by(title: movie_title)
+    end
   end
-end
 
   def has_existing_rating?(user_id, movie_id)
     Rating.find_by(user_id:, movie_id:)
