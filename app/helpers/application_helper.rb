34c1556e2fff663fcb2124667828db9a1a289080
Matt Seidholz <mseidholz@gmail.com>
2024-02-17 10:11:49 -0500

    generalize rating grouping

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  module RatingsHelper
-    def matt
-      @matt ||= User.find_by(name: 'Matt')
-    end
-
-    def rebecca
-      @rebecca ||= User.find_by(name: 'Rebecca')
-    end
-  end
 end
