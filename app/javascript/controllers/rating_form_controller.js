348d4968f88a3ba90cebd7603f20a480e2b38dcc
Matt Seidholz <mseidholz@gmail.com>
2024-02-18 12:17:39 -0500

    complete form switching

--- app/javascript/controllers/rating_form_controller.js
+++ app/javascript/controllers/rating_form_controller.js
@@ -1,11 +1,21 @@
 import { Controller } from '@hotwired/stimulus';
 
 export class RatingForm extends Controller {
-  static targets = ['new', 'existing'];
+  static targets = ['new', 'existing', 'toggleButton'];
 
   toggle_movie_form(e) {
     e.preventDefault();
+    this.toggleButtonText();
     this.existingTarget.classList.toggle('hidden');
     this.newTarget.classList.toggle('hidden');
+  }
+
+  toggleButtonText() {
+    const button = this.toggleButtonTarget;
+    if (button.textContent === 'Add a new movie') {
+      button.textContent = 'Rate an existing movie';
+    } else {
+      button.textContent = 'Add a new movie';
+    }
   }
 }
