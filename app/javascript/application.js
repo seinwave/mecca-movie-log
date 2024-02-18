502619baf04f581eb55070ed0ee06127658ac2c4
Matt Seidholz <mseidholz@gmail.com>
2024-02-18 13:22:45 -0500

    add yarn.rake task

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require_tree ./controllers
 import '@hotwired/turbo-rails';
 import 'controllers';
