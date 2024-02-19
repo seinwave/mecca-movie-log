6b35a54713c82aba7af73f64fa33158e1d7f6048
Matt Seidholz <mseidholz@gmail.com>
2024-02-19 07:11:00 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   post '/ratings', to: 'ratings#create', as: 'add_rating'
   post 'new_movie', to: 'ratings#new_movie', as: 'new_movie'
   post 'select_movie', to: 'ratings#select_movie', as: 'select_movie'
-  
 
   # Defines the root path route ("/")
   root 'ratings#index'
